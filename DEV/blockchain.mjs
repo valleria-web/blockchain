d3bd6800644ac428dfa39b2fafad9d3a67bf2b5a
vdsr <valleria@gmail.com>
2024-05-06 15:20:31 +0200

    first commit

---
+++ DEV/blockchain.mjs
@@ -0,0 +1,38 @@
+class Blockchain {
+  constructor() {
+    this.chain = [];
+    this.pendingTransactions = [];
+  }
+
+  createNewBlock(nonce, previousBlockHash, hash) {
+    const newBlock = {
+      index: this.chain.length + 1,
+      timestamp: Date.now(),
+      transactions: this.pendingTransactions,
+      nonce: nonce,
+      hash: hash,
+      previousBlockHash: previousBlockHash,
+    };
+    this.pendingTransactions = [];
+    this.chain.push(newBlock);
+
+    return newBlock;
+  }
+
+  getLastBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  createNewTransaction(amount, sender, recipient) {
+    const newTransaction = {
+      amount: amount,
+      sender: sender,
+      recipient: recipient,
+    };
+    this.pendingTransactions.push(newTransaction);
+
+    return this.getLastBlock()["index"] + 1;
+  }
+}
+
+export default Blockchain;
---
+++ DEV/test.mjs
@@ -0,0 +1,9 @@
+//[] {}
+
+import Blockchain from './blockchain.mjs';
+
+const blockchain = new Blockchain();
+blockchain.createNewBlock(2355, "HSGGGIWEA", "YUTSSSPAWW");
+blockchain.createNewBlock(4521, "SFDFSWEA", "SDFSFSPAWW");
+blockchain.createNewBlock(7854, "HTTNND77IWEA", "YDDFWWWQXDCV");
+console.log(blockchain);
